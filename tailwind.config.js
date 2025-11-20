d6b041ee2f7eeb347eaf5e3384cd0ef4b39c7b83
Tala Tayebi <tala@cloudflare.com>
2025-11-20 17:08:22 +0000

    Update brand color in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: '#ffd83aff',
+          dark: '#1D5FCC',
+          light: '#A8C9FF'
+        }
+      }
+    }
   },
   plugins: [],
 }
